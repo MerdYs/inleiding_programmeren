633da78c038a323414d29839856f7e49e032b8c8
Mert <mertbabur@hotmail.nl>
2025-03-14 02:55:14 +0100

    pizza2

--- script.js
+++ script.js
@@ -1,26 +1,9 @@
-let toppings = [];
+function addPepperoni(choice) {
+    let pepperoniImg = document.getElementById("pizza-pepperoni");
 
-function addTopping(topping) {
-    if (!toppings.includes(topping)) {
-        toppings.push(topping);
-        document.getElementById('slice-sound').play();
-        updatePizzaImage();
-        document.getElementById(topping + '-btn').disabled = true;
+    if (choice) {
+        pepperoniImg.style.display = "block"; // Pepperoni tonen
+    } else {
+        pepperoniImg.style.display = "none"; // Terug naar de standaard pizza
     }
-    if (toppings.length === 3) {
-        document.getElementById('bake-btn').style.display = 'block';
-    }
-}
-
-function updatePizzaImage() {
-    let imgSrc = "images/pizza_base";
-    if (toppings.includes("cheese")) imgSrc += "_cheese";
-    if (toppings.includes("pepperoni")) imgSrc += "_cheese_pepperoni";
-    if (toppings.includes("mushrooms")) imgSrc += "_cheese_pepperoni_mushrooms";
-    document.getElementById("pizza").src = imgSrc + ".png";
-}
-
-function bakePizza() {
-    document.getElementById("bake-sound").play();
-    document.getElementById("pizza").src = "images/pizza_baked.png";
 }
