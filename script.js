f396783b972611fd57070c97ea7720bd77bc5a95
Mert <mertbabur@hotmail.nl>
2025-03-20 01:19:04 +0100

    cleanup

--- script.js
+++ script.js
@@ -1,14 +1,3 @@
-// function addPepperoni(choice) {
-//     let pepperoniImg = document.getElementById("pepperoni");
-
-//     if (choice) {
-//         pepperoniImg.style.display = "block"; // Pepperoni tonen
-//     } else {
-//         pepperoniImg.style.display = "none"; // Terug naar de standaard pizza
-//     }
-// }
-
-// Vooraf laden van de geluiden
 const addSound = new Audio("sounds/toegevoegd.mp3");
 const removeSound = new Audio("sounds/verwijderd.mp3");
 
@@ -18,12 +7,12 @@ document.addEventListener("DOMContentLoaded", () => {
     const pepperoni = document.getElementById("pepperoni");
 
     yesButton.addEventListener("click", () => {
-        pepperoni.style.display = "block"; // Pepperoni zichtbaar maken
+        pepperoni.style.display = "block";
         addSound.play();
     });
 
     noButton.addEventListener("click", () => {
-        pepperoni.style.display = "none"; // Pepperoni verbergen
+        pepperoni.style.display = "none";
         removeSound.play();
     });
 });
