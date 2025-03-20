bc09f5aeb43c272bf77d9e9114a3852687bb34ae
Mert <mertbabur@hotmail.nl>
2025-03-20 23:19:28 +0100

    after feedback

--- script.js
+++ script.js
@@ -1,20 +1,36 @@
+//https://stackoverflow.com/questions/9419263/how-to-play-audio
 const addSound = new Audio("sounds/toegevoegd.mp3");
 const removeSound = new Audio("sounds/verwijderd.mp3");
 
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", function () {
     const yesButton = document.getElementById("yes");
     const noButton = document.getElementById("no");
     const pepperoni = document.getElementById("pepperoni");
 
-    yesButton.addEventListener("click", () => {
-        pepperoni.style.display = "block";
-        addSound.play();
+    noButton.disabled = true;
+
+    yesButton.addEventListener("click", function () {
+        if (!yesButton.disabled) { 
+            pepperoni.style.display = "block";
+            playSound("sounds/toegevoegd.mp3");
+            
+            yesButton.disabled = true;
+            noButton.disabled = false;
+        }
     });
 
-    noButton.addEventListener("click", () => {
-        pepperoni.style.display = "none";
-        removeSound.play();
+    noButton.addEventListener("click", function () {
+        if (!noButton.disabled) {
+            pepperoni.style.display = "none";
+            playSound("sounds/verwijderd.mp3");
+
+            yesButton.disabled = false;
+            noButton.disabled = true;
+        }
     });
+
+    function playSound(src) {
+        const sound = new Audio(src);
+        sound.play();
+    }
 });
-
-
